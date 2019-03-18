95eeae6fac310f6ada2adab5a64e8c060f4ae7ce
Peejay Garcia <peejgarcia@gmail.com>
2019-03-17 21:02:06 -0500

    Working on it.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -110,6 +111,14 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+
+        if (someJobs.isEmpty()){
+            System.out.println("Sorry, there is nothing to display.");
+        } else {
+            for(int i = 0; )
+
+
+        }
 
         System.out.println("printJobs is not implemented yet");
     }
